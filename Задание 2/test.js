cfc4400329dc242e14464a665048de0d6f57504d
Papa1881 <papaleon18@gmail.com>
2025-05-21 11:56:56 +0500

    Add files via upload

---
+++ Задание 2/test.js
@@ -0,0 +1,5 @@
+function showScreenSize() {
+    const width = window.screen.width;
+    const height = window.screen.height;
+    alert(`Ширина экрана: ${width}px\nВысота экрана: ${height}px`);
+  }
